d30901eadfe703b62f4b339393c6e1c80b0286cc
Yi-Ning <ning19950212@gmail.com>
2022-03-04 14:21:37 +0800

    react-test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import config from './Config';
 
 function App() {
+
+  const [data,setData] = useState({})
+
+  useEffect(() => {
+
+    (async ()=>{
+      const r1 = await fetch(config.AB_LIST);
+      const obj = await r1.json();
+      console.log(obj);
+      setData(obj);
+    })();
+  // fetch(config.AB_LIST).then(r=>r.json()).then(obj=>{
+  //   console.log(obj);
+  //   setData(obj);
+  //   console.log(obj);
+  }, []);
+  console.log(data);
+
+  const renderMe = (data)=>{
+    if(data.rows && data.rows.length){
+      return data.rows.map((v,i)=>
+    <tr key={v.sid}>
+      <td>{v.sid}</td>
+      <td>{v.name}</td>
+      <td>{v.email}</td>
+      <td>{v.mobile}</td>
+      <td>{v.birthday}</td>
+    </tr>
+    )
+  }else{
+    return (<tr><td></td></tr>);
+  };
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+    <table className="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">name</th>
+      <th scope="col">email</th>
+      <th scope="col">mobile</th>
+      <th scope="col">birthday</th>
+    </tr>
+  </thead>
+  <tbody>
+    { renderMe(data)}
+  </tbody>
+</table>
+    </div>
     </div>
   );
 }
---
+++ src/Config.js
@@ -0,0 +1,5 @@
+const host = 'http://localhost:3500';
+const config ={
+    AB_LIST:`${host}/address-book/api/list`
+};
+export default config;
